4d65e623bd296d5faa8651261b63303c627fb46b
wukaichao <1554172395@qq.com>
2022-09-26 17:15:56 +0800

    spring-secutiry

--- community/src/main/java/com/nowcoder/community/entity/DiscussPost.java
+++ community/src/main/java/com/nowcoder/community/entity/DiscussPost.java
@@ -1,41 +1,29 @@
 package com.nowcoder.community.entity;
 
 import org.springframework.data.annotation.Id;
-import org.springframework.data.elasticsearch.annotations.Document;
-import org.springframework.data.elasticsearch.annotations.Field;
-import org.springframework.data.elasticsearch.annotations.FieldType;
+
 
 import java.util.Date;
 
-@Document(indexName = "discusspost", type = "_doc", shards = 6, replicas = 3)
+//@Document(indexName = "discusspost", type = "_doc", shards = 6, replicas = 3)
 public class DiscussPost {
 
-    @Id
     private int id;
 
-    @Field(type = FieldType.Integer)
     private int userId;       //用户ID
-
     // 互联网校招
-    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     private String title;     //帖子标题
 
-    @Field(type = FieldType.Text, analyzer = "ik_max_word", searchAnalyzer = "ik_smart")
     private String content;   //帖子内容
 
-    @Field(type = FieldType.Integer)
     private int type;         //帖子类型  0：普通  1：置顶
 
-    @Field(type = FieldType.Integer)
     private int status;       //帖子状态  0：正常  1：精华  2：拉黑
 
-    @Field(type = FieldType.Date)
     private Date createTime;  //帖子创建的日期
 
-    @Field(type = FieldType.Integer)
     private int commentCount; //帖子评论的数量
 
-    @Field(type = FieldType.Double)
     private  double score;    //帖子的分数，区分热度
 
     public int getId() {
